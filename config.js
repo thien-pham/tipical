34057c7e81281e567f44c6ea36eb457c8e546b61
Zach Williams <saumanahaii@gmail.com>
2017-05-09 14:13:16 -0400

    changed config to fix travis

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 exports.DATABASE = process.env.DATABASE_URL ||
-                   global.DATABASE_URL;
+                   global.DATABASE_URL || 'mongodb://localhost/tipical';
 
 exports.PORT = process.env.PORT || 8080;
-
