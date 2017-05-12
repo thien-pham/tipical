f72af11bf1436da746df50c5971d48c3baeec955
Zach Williams <saumanahaii@gmail.com>
2017-05-12 12:38:01 -0400

    working on authentication

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use('/app',express.static('views'));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", req.get('origin'));
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
