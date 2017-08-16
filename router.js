1e4526d7e820c35750e4477ef8657aa66680e90f
pham-thien <pham.thien@outlook.com>
2017-08-16 18:33:25 -0400

    adding in auth

--- router.js
+++ router.js
@@ -3,6 +3,30 @@ let {Tips, User} = require('./models');
 
 mongoose.Promise = global.Promise;
 
+const strategy = new BasicStrategy(function(username, password, callback) {
+  let user;
+  User
+    .findOne({username: username})
+    .exec()
+    .then(_user => {
+      user = _user;
+      if (!user) {
+        return callback(null, false, {message: 'Incorrect username'});
+      }
+      return user.validatePassword(password);
+    })
+    .then(isValid => {
+      if (!isValid) {
+        return callback(null, false, {message: 'Incorrect password'});
+      }
+      else {
+        return callback(null, user);
+      }
+    });
+});
+
+passport.use(strategy);
+
 //the closure for the module exporting
 module.exports = (app) => {
   const bodyParser = require('body-parser');
@@ -67,8 +91,42 @@ module.exports = (app) => {
     });
   });
 
+  //For client-side logging in info.  Ping it and if valid, returns true
+app.get('/users',passport.authenticate('basic',{session:false}),(req,res)=>{
+  console.log("Hey there!");
+  res.send(true);
+});
+
   //POST ENDPOINTS-------------------------------------------------------
   //create post endpoint
+
+  //Create user endpoint
+app.post('/users', (req, res) => {
+  return User
+    .find({username: req.body.username})
+    .count()
+    .exec()
+    .then(count => {
+      if (count > 0) {
+        return res.status(422).json({message: 'username already taken'});
+      }
+      return User.hashPassword(req.body.password);
+    })
+    .then(hash => {
+      return User
+        .create({
+          username: req.body.username,
+          password: hash
+    })
+    .then(user => {
+      return res.status(201).json(user);
+    })
+    .catch(() => {
+      res.status(500).json({message: 'something went terribly awry'});
+    });
+  });
+});
+
   app.post('/posts', (req, res) => {
     console.log('does this hit', req.body);
     const lat = parseFloat(req.body.location[0]);
@@ -87,6 +145,16 @@ module.exports = (app) => {
       });
   });
 
+  //UPDATE ENDPOINTS-------------------------------------------------------
+  //update user endpoint
+  app.put('/posts/:id',
+    passport.authenticate('basic', {session:false}), (req, res) => {
+    Tips
+      .findByIdAndUpdate(req.params.id, {$set: req.body})
+      .then(() => res.location('/me').status(204).end())
+      .catch(() => res.status(500).json({message: 'Internal server error on put'}));
+  });
+  
   //DELETE ENDPOINTS-------------------------------------------------------
   app.delete('/posts/:id', (req, res) => {
     Tips
