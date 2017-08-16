c9f4be2abe0bccf23f951d8b3458c9013c52335b
pham-thien <pham.thien@outlook.com>
2017-08-16 01:57:15 -0400

    styling

--- views/app.js
+++ views/app.js
@@ -89,19 +89,19 @@ $(document).ready(function(){
           lng: tip.location[1]
         };
         console.log('???', center)
-        // var image = {
-        //   url: 'marker.png',
-        //   // This marker is 20 pixels wide by 32 pixels high.
-        //   size: new google.maps.Size(32, 32),
-        //   // The origin for this image is (0, 0).
-        //   origin: new google.maps.Point(0, 0),
-        //   // The anchor for this image is the base of the flagpole at (0, 32).
-        //   anchor: new google.maps.Point(0, 32)
-        // };
+        var image = {
+          url: 'marker.png',
+          // This marker is 20 pixels wide by 32 pixels high.
+          size: new google.maps.Size(32, 32),
+          // The origin for this image is (0, 0).
+          origin: new google.maps.Point(0, 0),
+          // The anchor for this image is the base of the flagpole at (0, 32).
+          anchor: new google.maps.Point(0, 32)
+        };
         var pin = new google.maps.Marker({
           position: center,
           map,
-          icon: 'marker.png',
+          icon: image,
           label: tip.body});
         markers.push(pin)
         console.log('making a marker');
